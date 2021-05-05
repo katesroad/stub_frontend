98f014501a747281070bf36268c4e662d1291dbe
Kate <test@gmail.com>
2021-05-05 00:32:13 -0400

    set up test utils

--- src/test/server/handlers.ts
+++ src/test/server/handlers.ts
@@ -1,1 +1,9 @@
-export const handlers = []
+import { rest } from 'msw'
+
+const apiUrl = (path: string) => process.env.REACT_APP_API_URL + '/' + path
+
+export const handlers = [
+  rest.get(apiUrl('auth/token'), async (req, res, ctx) => {
+    return res(ctx.json({ id: Date.now() }))
+  }),
+]
--- src/test/server/index.ts
+++ src/test/server/index.ts
@@ -1,7 +1,2 @@
-import { server as devServer } from './dev-server'
-import { server as testServer } from './test-server'
-
-const isTestEnv = process.env.NODE_ENV === 'test' ? true : false
-
-export const server = isTestEnv ? testServer : devServer
+export * from './server'
 export * from 'msw'

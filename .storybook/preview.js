758ef699e7fe64ac414114760e046204d4cd910d
Kate <test@gmail.com>
2021-05-07 22:33:03 -0400

    config storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import AppProviders from '../src/context/index'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [(Story) => <AppProviders><Story /></AppProviders>]
--- src/components/AuthForm/index.tsx
+++ src/components/AuthForm/index.tsx
@@ -4,7 +4,7 @@ import { initialValues, AuthSchema } from './auth.helper'
 import { Wrapper } from './styles'
 import { UseMutationResult } from 'react-query'
 import { User } from 'types'
-import { TextField } from 'components/lib/form/TextField'
+import { Input } from 'components/lib/form/Input'
 import { Button, Error } from 'components/lib'
 
 type AuthFormProps = {
@@ -36,23 +36,21 @@ export default function AuthForm({ type, mutation }: AuthFormProps) {
             <Form onFocus={getClearErrorHandler(props)}>
               <h2 className="form-title">{type}</h2>
               {type === 'register' ? (
-                <TextField
+                <Input
                   label="user name"
                   name="username"
                   placeholder="user name"
                 />
               ) : null}
-              <TextField label="email" name="email" placeholder="email" />
-              <TextField
+              <Input label="email" name="email" placeholder="email" />
+              <Input
                 label="password"
                 name="password"
                 placeholder="password"
                 type="password"
               />
               <p>
-                <Button type="submit" className="btn--submit">
-                  {type}
-                </Button>
+                <Button type="submit">{type}</Button>
               </p>
               <Error as="p">{errMsg}</Error>
             </Form>

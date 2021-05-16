7ae5cebb1168067373576c0e30eba37813012d72
Kate <test@gmail.com>
2021-05-16 00:15:22 -0400

    layout componnet

---
+++ src/components/commom/layout/index.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { Header } from '../Header'
+import { Footer } from '../Footer'
+import { Content } from '../styled'
+import { LayoutContent, Title } from './styles'
+
+interface AppLayoutProps {
+  title?: string | React.ReactNode
+}
+
+export const AppLayout: React.FC<AppLayoutProps> = ({
+  children,
+  title,
+  ...props
+}) => {
+  return (
+    <>
+      <Header />
+      <Content style={{ flexGrow: 1 }} as="main">
+        {title ? (
+          <Title>
+            {typeof title === 'string' ? <h2>{title}</h2> : <>{title}</>}
+          </Title>
+        ) : null}
+        <LayoutContent {...props}>{children}</LayoutContent>
+      </Content>
+      <Footer />
+    </>
+  )
+}
+
+export const Layout: React.FC = ({ children }) => (
+  <main style={{ flexGrow: 1 }}>{children}</main>
+)

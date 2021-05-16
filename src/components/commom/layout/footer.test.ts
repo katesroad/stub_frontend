392aceda71890744a15968849295322454cb61d4
Kate <test@gmail.com>
2021-05-16 14:15:43 -0400

    delete not used file

--- src/components/commom/layout/footer.test.ts
+++
@@ -1,4 +0,0 @@
-import { render, AppProviders } from 'test/utils'
-
-test.todo('test with user logined')
-test.todo('test without user logined')
--- src/components/commom/layout/footer.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render, AppProviders } from 'test/utils'
-import { Footer } from './footer'
-
-test('render <Footer /> without crash', () => {
-  render(<Footer />, { wrappers: AppProviders })
-})
